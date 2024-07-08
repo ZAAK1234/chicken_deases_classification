19f6382f6879642dcc65fe8644a995b2697d85f5
Fahim <fjaragal@gmail.com>
2024-07-08 23:24:00 +0530

    file structure

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#Todo:change project name as your project
+project_name='chicken_deases_classification'
+# when we work on deployment then we give CICD related command
+#gitkeep for empty file
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/componenets/__init__.py",
+    f"src/{project_name}/utils.py/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constant/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the file {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Creating empty {filepath}")
+    else:
+        logging.info(f"File {filename} is already exists")
