1f46f17bfeb017193a22a6469b7fc92dfcb7014e
Fahim <fjaragal@gmail.com>
2024-07-12 23:51:29 +0530

    Prepare callbacks added

---
+++ src/chicken_deases_classification/componenets/prepared_callbacks.py
@@ -0,0 +1,33 @@
+# components
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+from src.chicken_deases_classification.entity.config_entity import PrepareCallbackConfig
+
+
+class PrepareCallback:
+    def __init__(self, config: PrepareCallbackConfig):
+        self.config = config
+
+    @property
+    def _create_tb_callback(self):
+        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        tb_running_log_dir = os.path.join(self.config.tensorboard_root_log_dir, f"tb_log_at_{timestamp}"
+                                          )
+        return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+
+    @property
+    def _create_ckpt_callback(self):
+        return tf.keras.callbacks.ModelCheckpoint(
+
+            filepath=str(self.config.checkpoint_model_filepath),
+            save_best_only=True
+        )
+
+    def get_tb_ckpt_callback(self):
+        return [
+            self._create_tb_callback,
+            self._create_ckpt_callback
+        ]
--- src/chicken_deases_classification/config/configuration.py
+++ src/chicken_deases_classification/config/configuration.py
@@ -1,7 +1,8 @@
 from src.chicken_deases_classification.constant import *
 from src.chicken_deases_classification.utils.common import read_yaml,create_directories
 from src.chicken_deases_classification.entity.config_entity import DataIngestionConfig
-from src.chicken_deases_classification.entity.config_entity import PrepareModelBaseConfig
+from src.chicken_deases_classification.entity.config_entity import PrepareModelBaseConfig,PrepareCallbackConfig
+import os
 class ConfigurationManager:
     def __init__(
             self,
@@ -39,4 +40,20 @@ class ConfigurationManager:
             params_weights=self.params.WEIGHTS,
             params_classes=self.params.CLASSES)
         return prepare_base_model_config
+
+    def get_prepare_callbacks_config(self) -> PrepareCallbackConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbackConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
 
